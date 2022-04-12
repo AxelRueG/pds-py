b182da47800ba16588c4f4e1ebca548cd4b5e9cb
Axel Rueda <axelcuer@gmail.com>
2022-04-12 17:22:13 -0300

    convolucion circular sin funcionar

--- guia2.py
+++ guia2.py
@@ -1,7 +1,7 @@
 import numpy as np 
 import matplotlib.pyplot as plt
-
 from Signals import cuadrada, senoidal
+
 def ejer1 ():
   # ejercicio 1 _______________________________________________________________
   # item 1
@@ -38,8 +38,37 @@ def respuesta_al_impulso(n, b, a):
   plt.show()
   
 
+def convolucion_lineal(x,h):
+  N = x.size+h.size-1
+  y = np.zeros(N)
+  x1 = np.append(x, np.zeros(h.size-1))
+  h1 = np.append(h, np.zeros(x.size-1))
+  for k in range(0,N):
+    y[k] = sum(x1[0:N]*h1[k-np.arange(0,N)])
+
+  return y
+
+def convolucion_circular(x,h):
+  N = h.size
+  y = np.zeros(N)
+  for k in range(0,N):
+    # y[k] = np.dot(x1[k:k+N],h[h.size::-1])
+    for l in range(0,N):
+      print(l, '  ', (N+k-l)%N)
+      y[k] += h[l]*x[((N+k-l)%N)]
+
+  return y
+
+
+
+
 if __name__ == '__main__':
-  respuesta_al_impulso(25, np.array([1]), np.array([-1]))
-  respuesta_al_impulso(25, np.array([1,0.5]), np.array([0]))
-  respuesta_al_impulso(25, np.array([1]), np.array([0.5,-0.25]))
+  # respuesta_al_impulso(25, np.array([1]), np.array([-1]))
+  # respuesta_al_impulso(25, np.array([1,0.5]), np.array([0]))
+  # respuesta_al_impulso(25, np.array([1]), np.array([0.5,-0.25]))
   
+  # y = convolucion_lineal(np.array([1,2,3,5,6]),np.array([1,0,0]))
+  y = convolucion_circular(np.array([2,1,1.5,2,1.5,1]),np.array([3,2,1]))
+  print(y)
+  # plt.stem(y)
+  # plt.show()
